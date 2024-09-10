1ba4f14d0c165ddee5e40fa62b2db43b04da524b
Cauã de Lima Araújo <20231144010003@ifrn.local>
2024-09-10 10:13:09 -0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,20 @@
+const menuActions = document.querySelector('.menu-actions')
 const computerMenu_BTN = document.getElementById('computer-btn');
 const storageMenu_BTN = document.getElementById('storage-btn');
 const computerWindow = document.querySelector('.computer-window');
 const storageWindow = document.querySelector('.storage-window');
 
 computerMenu_BTN.addEventListener('click', () => {
-    computerWindow.classList.toggle('altShow_Window');
+    computerWindow.classList.toggle('window_AltShow');
 });
 
 storageMenu_BTN.addEventListener('click', () => {
-    storageWindow.classList.toggle('altShow_Window');
+    storageWindow.classList.toggle('window_AltShow');
 });
 
 function closeWindow({ target }){
     const targetParent = target.parentElement.parentElement
-    targetParent.classList.toggle('altShow_Window')
+    targetParent.classList.toggle('window_AltShow')
 };
 
 let currentMovingWindow
@@ -22,7 +23,13 @@ let offsetX, offsetY;
 
 function moveWindow({ clientX, clientY, target}){
     isDragging = true
+
+    if (currentMovingWindow){
+        currentMovingWindow.classList.remove('window_AboveAll')
+    }
+
     currentMovingWindow = target.parentElement
+    currentMovingWindow.classList.add('window_AboveAll')
     offsetX = clientX - currentMovingWindow.getBoundingClientRect().left;
     offsetY = clientY - currentMovingWindow.getBoundingClientRect().top;
 };
@@ -31,8 +38,18 @@ document.addEventListener('mousemove', ({ clientX, clientY }) => {
     if (isDragging) {
         const x = clientX - offsetX;
         const y = clientY - offsetY;
-        currentMovingWindow.style.left = `${x}px`;
-        currentMovingWindow.style.top = `${y}px`;
+
+        if (x <= menuActions.clientWidth){
+            currentMovingWindow.style.left = `${menuActions.clientWidth}px`;
+
+        } else if (y <= 0) {
+            currentMovingWindow.style.top = '0px';
+
+        } else {
+            currentMovingWindow.style.left = `${x}px`
+            currentMovingWindow.style.top = `${y}px`;
+        }
+        
     }
 });
 
